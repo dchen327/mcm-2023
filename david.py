6e2e4d36e648b99f3fb12d2def0762a04a6f4514
David Chen <davidc3287@gmail.com>
2023-02-19 23:20:18 -0800

    graphs

--- david.py
+++ david.py
@@ -7,9 +7,9 @@ df.head()
 print(df.columns)
 # %%
 df['Pct_Hard_Mode'] = df['Number_Hard_Mode'] / df['Num_Rep_Results']
+df['Date'] = pd.to_datetime(df['Date'])
 df.head()
 
-# visualize all column distributions
-for col in df.columns:
-    fig = px.histogram(df, x=col, nbins=100)
-    fig.show()
+# visualize Num_Rep_Results over time
+fig = px.line(df, x='Date', y='Num_Rep_Results', title='Number of Rep Results')
+fig.show()
