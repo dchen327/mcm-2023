0ed16dcf59a417b87bdc5ac3013e80105404c926
David Chen <davidc3287@gmail.com>
2023-02-20 13:49:50 -0800

    word list

--- david.py
+++ david.py
@@ -13,3 +13,15 @@ df.head()
 # visualize Num_Rep_Results over time
 fig = px.line(df, x='Date', y='Num_Rep_Results', title='Number of Rep Results')
 fig.show()
+# %%
+# Feature generation
+# uncommon letters are ZQJXKVBYWGP
+def has_uncommon_letters(word):
+    uncommon_letters = set('zqjxkvbywgp')
+    return any(letter in uncommon_letters for letter in word)
+
+df['Has_Uncommon_Letters'] = df['Word'].apply(has_uncommon_letters)
+
+
+
+# %%
