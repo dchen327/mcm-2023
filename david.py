294d29a836a8a5f77330fb757f414e6bc9e5592a
David Chen <davidc3287@gmail.com>
2023-02-19 23:14:23 -0800

    new data

--- david.py
+++ david.py
@@ -1,7 +1,15 @@
 #%%
 import pandas as pd
+import plotly.express as px
 
 df = pd.read_csv('wordle.csv')
 df.head()
+print(df.columns)
 # %%
-df['pct_hard_mode'] = df['hard_mode'] / df['total']
+df['Pct_Hard_Mode'] = df['Number_Hard_Mode'] / df['Num_Rep_Results']
+df.head()
+
+# visualize all column distributions
+for col in df.columns:
+    fig = px.histogram(df, x=col, nbins=100)
+    fig.show()
